253bbdb0f72ff44b476e27eb713c0326d2e29d2a
Jose A <jose.garcia@pixelabs.es>
2018-05-04 11:46:54 +0200

    No default features in STRUCK

--- STRUCK/BasicConfig.hpp
+++ STRUCK/BasicConfig.hpp
@@ -1,6 +1,8 @@
 #ifndef BASICCONFIG_H_ST
 #define BASICCONFIG_H_ST
+
 #include <sstream>
+#include <vector>
 
 void add(std::ostream& ss, const std::string& key, const std::string& value)
 {
@@ -8,8 +10,21 @@ void add(std::ostream& ss, const std::string& key, const std::string& value)
         ss << key << " = " << value << '\n';
 }
 
+/*
+image features to use.
+format is: feature kernel [kernel-params]
+where:
+  feature = haar/raw/histogram
+  kernel = gaussian/linear/intersection/chi2
+  for kernel=gaussian, kernel-params is sigma
+multiple features can be specified and will be combined
+feature = haar gaussian 0.2
+#feature = raw gaussian 0.1
+#feature = histogram intersection
+*/
+
 std::stringstream buildBasicConf(const std::string& sPath, const std::string& sName,
-                                 const std::string& rPath, const std::string& features,
+                                 const std::vector<std::string>& features, const std::string& rPath = "",
                                  const std::string& fWidth = "320", const std::string& fHeight = "240",
                                  const std::string& quietMode = "0", const std::string& debugMode = "1",
                                  const std::string& seed = "0", const std::string& sRadius = "30",
@@ -28,6 +43,7 @@ std::stringstream buildBasicConf(const std::string& sPath, const std::string& sN
     add(ss, "searchRadius", sRadius);
     add(ss, "svmC", svmC);
     add(ss, "svmBudgetSize", bSize);
+    for (auto feat: features) add(ss, "feature", feat);
     return ss;    
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+// #include <sstream>
 #include <string>
+#include <vector>
 #include "../STRUCK/BasicConfig.hpp"
 
 
 int main(int argc, char** argv)
 {
-
-    auto a = buildBasicConf("run/to/the/hills", "baby", "", "these are the features I like");
+    std::vector<std::string> features {"haar gaussian 0.2", "histogram gaussian 0.1", "haar linear"};
+    auto configurationString = buildBasicConf("run/to/the/hills", "baby", features);
     std::string line;
-    while (getline(a, line))
+    while (getline(configurationString, line))
     {
         std::cout << line << '\n';
     }
